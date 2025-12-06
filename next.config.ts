8b809b76178e85f688239ad7af141143ecc4211a
ho3ein1379 <ariafardhossein@gmail.com>
2025-12-06 19:12:45 +0330

    improved next.config.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
+const rawUrl = process.env.NEXT_PUBLIC_API_URL || "";
+const host = rawUrl.replace("http://", "").replace("https://", "");
+
+const [hostname = "", port = ""] = host.split(":");
+
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "http",
+        hostname,
+        port,
+        pathname: "/uploads/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
